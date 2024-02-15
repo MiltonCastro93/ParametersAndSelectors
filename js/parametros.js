b0ae8438d2188548cf9a126e3f9e8cc537e0c286
Milton Denis Castro <159483393+MiltonCastro93@users.noreply.github.com>
2024-02-15 15:34:09 -0300

    Update parametros.js

--- js/parametros.js
+++ js/parametros.js
@@ -1,8 +1,7 @@
-//Ubicamos elemento dentro del DOM
 const caja = document.querySelector('#caja');
 const picker = document.querySelector('#picker');
 
-//declaramos funciones
+
 function saludar(nombre){
     console.log( 'Hola ' + nombre);
 }
@@ -12,12 +11,9 @@ function pintar( codigoColor ){
 }
 
 function colorear(){
-    //obtenemos el codigo de color que seleccione el usuario en el picker
     let colorSecciona = picker.value;
-    //invocamos la funcion pintar()
     pintar( colorSecciona );
 
 }
 
-//invocamos la funcion
 saludar('milton');
