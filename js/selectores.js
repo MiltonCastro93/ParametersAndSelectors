b9697eaf9f6e51be3758582df4751166796e0b08
Milton Denis Castro <159483393+MiltonCastro93@users.noreply.github.com>
2024-02-15 15:33:55 -0300

    Update selectores.js

--- js/selectores.js
+++ js/selectores.js
@@ -1,10 +1,8 @@
 const caja = document.getElementById('caja');
-//const caja = document.querySelector('#caja');
 
-//const lis = document.getElementsByTagName('li');
 const lis = document.querySelectorAll('li');
 lis[0].style.fontSize = '32';
 
 const verde = document.getElementsByClassName('verde');
-//const verde = document.querySelectorAll('.verde');
+
 verde[1].style.borde = '3px solid #fff';
